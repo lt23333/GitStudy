7a3f518169fa67302169dccd6b08dfd52481d27d
litao <2397933486@qq.com>
2023-03-28 12:19:18 +0800

    2 commit

--- src/main/java/org/example/gitstudy.java
+++ src/main/java/org/example/gitstudy.java
@@ -3,5 +3,6 @@ package org.example;
 public class gitstudy {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("heelo git2");
     }
 }
